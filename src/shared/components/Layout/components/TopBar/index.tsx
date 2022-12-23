a61473c6a0b95444df6d5090703cc17a12625cd8
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2022-12-23 10:24:41 -0300

    Feat: inserta styles topBar

--- src/shared/components/Layout/components/TopBar/index.tsx
+++ src/shared/components/Layout/components/TopBar/index.tsx
@@ -1,21 +1,66 @@
-import { Container, Content, Form, Img } from './styles';
-import { Stack, IconButton } from '@mui/material';
-import { Menu } from '@mui/icons-material';
+import {
+  ButtonForIcon,
+  Container,
+  Content,
+  Form,
+  Img,
+  InputForm,
+} from './styles';
+import { Stack, IconButton, Button, Hidden } from '@mui/material';
+import {
+  Menu,
+  Search,
+  Apps,
+  MoreVert,
+  VideoCall,
+  AccountCircle,
+} from '@mui/icons-material';
 
 export const TopBar = () => {
   return (
     <Container color="default">
       <Content>
-        <Stack flexDirection={'row'} alignItems={'center'}>
+        <Stack flexDirection={'row'} alignItems={'center'} width="100%">
           <IconButton>
             <Menu />
           </IconButton>
           <Img src={'/new-youtube-logo.svg'} alt="" />
         </Stack>
-        <Stack>
-          <Form></Form>
+        <Hidden mdDown>
+          <Stack width="100%" alignItems={'center'}>
+            <Form component="form" elevation={0}>
+              <InputForm
+                placeholder="Pesquisar"
+                inputProps={{ 'aria-label': 'search google maps' }}
+              />
+              <ButtonForIcon type="submit" aria-label="search">
+                <Search />
+              </ButtonForIcon>
+            </Form>
+          </Stack>
+        </Hidden>
+        <Stack flexDirection={'row'} width="100%" justifyContent={'flex-end'}>
+          <ButtonForIcon>
+            <VideoCall />
+          </ButtonForIcon>
+          <ButtonForIcon>
+            <Apps />
+          </ButtonForIcon>
+          <ButtonForIcon>
+            <MoreVert />
+          </ButtonForIcon>
+          <Button
+            color="secondary"
+            component="a"
+            startIcon={<AccountCircle />}
+            variant="outlined"
+            // onClick={() => {
+            //   signIn("google")
+            // }}
+          >
+            Fazer Login
+          </Button>
         </Stack>
-        <Stack></Stack>
       </Content>
     </Container>
   );
